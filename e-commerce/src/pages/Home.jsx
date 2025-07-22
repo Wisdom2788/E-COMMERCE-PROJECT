32763fa0caf88563ac01d3a43b2a001f6cf96942
wisdom uzoma <princewizoma22@gmail.com>
2025-07-22 20:42:48 +0100

    commitment

--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -2,13 +2,13 @@ import heroImg from "../assets/hero.jpg";
 
 function Home() {
     return (
-        <section className="max-w-7xl max-auto px-4 py-12 flex flex-col md:flex-row items-center justify-between gap 8">
+        <section className="max-w-7xl mx-auto px-4 py-12 flex flex-col md:flex-row items-center justify-between gap-8 bg-gray">
             <div className="flex-1 flex flex-col items-start">
                 <h1 className="text-4xl md:text-6xl font-extrabold text-black leading-tight">
                     FIND CLOTHES <br/> THAT MATCHES <br/> YOUR STYLE
                 </h1>
-                <p className="text-gray-600 mt-4 mb-8 max-w-wd">
-                    Browse through our diverse range of meticulously crafted garments, designed <br/> to bring out your individuality and cater to youer sense of style.
+                <p className="text-gray-600 mt-4 mb-8 max-w-md">
+                    Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.
                 </p>
                 <button className="bg-black rounded-full text-white py-4 px-8 font-bold text-lg hover:bg-gray-900 transition mb-8">
                     Shop Now
@@ -19,12 +19,12 @@ function Home() {
                         <div className="text-gray-600">International Brands</div>
                     </div>
                     <div>
-                        <div className="text-2xl font-bold">2000+</div>
-                        <div classNmae="text-gray-600">High-Quality Products</div>
+                        <div className="text-2xl font-bold text-black">2,000+</div>
+                        <div className="text-gray-600">High-Quality Products</div>
                     </div>
                     <div>
-                        <div className="text-2xl font-bold text-black">30000+</div>
-                        <div className="tect-gray-600">Happy Customers</div>
+                        <div className="text-2xl font-bold text-black">30,000+</div>
+                        <div className="text-gray-600">Happy Customers</div>
                     </div>
                 </div>
             </div>
@@ -34,6 +34,6 @@ function Home() {
             </div>
         </section>
     );
-};
+}
 
 export default Home;
